265e8e248d20396bd95c0a7771b131fd0010d3db
Hazhelcj <hazhelcjmatingyad@gmail.com>
2024-09-10 15:12:50 +0300

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,24 @@
+package hazhel_tr;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int age = 18, black = 45, white = 3;
+	    String name = "Hazhel";
+	    Double gpa = 1.2;
+	    char grade = 'C';
+	    boolean ifs = true;
+	    
+	    System.out.print("Hello my name is " + name + " and I am " + age + " years old.");
+	       System.out.println(" I have a GPA of " + gpa + " and my grade is " + grade  );
+	       System.out.println("Graduated? " + ifs);
+	      System.out.println("Sum:" + black + white);
+	      System.out.println("Difference:" +  (black - white));
+	      System.out.println("Product:" + black * white);
+	       System.out.println("Quotient:" + black / white);
+	    
+
+
+	}
+
+}
